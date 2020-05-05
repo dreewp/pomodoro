006e99017aacde58822fe55bbe6d234b46e28406
Andrew Park <parkandrewkh@gmail.com>
2020-05-05 14:08:28 -0700

    Updates

---
+++ precache-manifest.e989f2f9ccfb2aa76364895f8dac9281.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c15b5468d2ef6875c734752f93a4d50",
+    "url": "/pomodoro/index.html"
+  },
+  {
+    "revision": "6f629e170f5827eb74f0",
+    "url": "/pomodoro/static/css/main.91cf4523.chunk.css"
+  },
+  {
+    "revision": "91d3d28c5fd229926fa5",
+    "url": "/pomodoro/static/js/2.d12cf8d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pomodoro/static/js/2.d12cf8d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f629e170f5827eb74f0",
+    "url": "/pomodoro/static/js/main.cd5288d8.chunk.js"
+  },
+  {
+    "revision": "be88404afcd08bafaf7e",
+    "url": "/pomodoro/static/js/runtime-main.a40df6f6.js"
+  },
+  {
+    "revision": "58045dabdc3a361cb9bb9faf2f1dd1f3",
+    "url": "/pomodoro/static/media/digital-7.58045dab.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9fa6a8007e723866fdb010d62807bf9b.js"
+  "/pomodoro/precache-manifest.e989f2f9ccfb2aa76364895f8dac9281.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pomodoro/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
